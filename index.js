b4475fdaff44cc45296789da366a77faff568d17
Matheus-Status-Code-200 <testeselenium@gmail.com>
2023-07-17 21:48:45 -0300

    ADD: MongoDB connection

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const userRoute = require("./src/routes/user.route")
+const connetcDatabase = require("./src/database/db")
+
 
 const port = 3000
+
+connetcDatabase();
 app.use(express.json());
 
 app.use('/user', userRoute);
--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 const connectDatabase = () => {
-    console.log("Wait.");
-    mongoose.connect()
-}
+    console.log("Wait connecting DB.");
+    mongoose.connect(
+            "mongodb+srv://testeselenium600:newsmongo@cluster0.mppsakt.mongodb.net/?retryWrites=true&w=majority", {
+                useUnifiedTopology: true
+            })
+        .then(() => console.log("CONNECTEDY MONGODB...."))
+        .catch((error) => console.log("CONNECTEDY MONGODB: " + error))
+};
+
+module.exports = connectDatabase;
