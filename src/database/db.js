79a12dbbe68fbbdf3938ae706936c9c88ee59972
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-07-31 21:45:45 -0300

    Add .env.

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+const apiUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const debugMode = process.env.DEBUG === 'true';
 const mongoose = require("mongoose");
+
 const connectDatabase = () => {
     console.log("Wait connecting DB.");
+    console.log("dados :" + apiUser + "" + dbPassword)
     mongoose.connect(
-            "mongodb+srv://testeselenium600:newsmongo@cluster0.mppsakt.mongodb.net/?retryWrites=true&w=majority", {
+            `mongodb+srv://${apiUser}:${dbPassword}@cluster0.mppsakt.mongodb.net/?retryWrites=true&w=majority`, {
                 useUnifiedTopology: true
             })
         .then(() => console.log("CONNECTEDY MONGODB...."))
